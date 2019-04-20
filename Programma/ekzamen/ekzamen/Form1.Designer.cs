5235e41f9aaacded3d943eba5fb30f839f6a4f8f
Максим Михеев <maksmixei09@gmail.com>
2019-04-20 16:02:23 +0300

    16:02

---
+++ Programma/ekzamen/ekzamen/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ekzamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.databaseDataSet = new ekzamen.databaseDataSet();
+            this.databaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.companyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.companyTableAdapter = new ekzamen.databaseDataSetTableAdapters.CompanyTableAdapter();
+            this.humanBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.humanTableAdapter = new ekzamen.databaseDataSetTableAdapters.HumanTableAdapter();
+            this.humanBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.humanBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.humanBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.companyBindingSource;
+            this.comboBox1.DisplayMember = "CompanyName";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(351, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.ValueMember = "CompanyName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Подбор сотрудника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Компания";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Подобрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "databaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // databaseDataSetBindingSource
+            // 
+            this.databaseDataSetBindingSource.DataSource = this.databaseDataSet;
+            this.databaseDataSetBindingSource.Position = 0;
+            // 
+            // companyBindingSource
+            // 
+            this.companyBindingSource.DataMember = "Company";
+            this.companyBindingSource.DataSource = this.databaseDataSetBindingSource;
+            // 
+            // companyTableAdapter
+            // 
+            this.companyTableAdapter.ClearBeforeFill = true;
+            // 
+            // humanBindingSource
+            // 
+            this.humanBindingSource.DataMember = "Human";
+            this.humanBindingSource.DataSource = this.databaseDataSetBindingSource;
+            // 
+            // humanTableAdapter
+            // 
+            this.humanTableAdapter.ClearBeforeFill = true;
+            // 
+            // humanBindingSource1
+            // 
+            this.humanBindingSource1.DataMember = "Human";
+            this.humanBindingSource1.DataSource = this.databaseDataSetBindingSource;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.humanBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.humanBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private databaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource databaseDataSetBindingSource;
+        private System.Windows.Forms.BindingSource companyBindingSource;
+        private databaseDataSetTableAdapters.CompanyTableAdapter companyTableAdapter;
+        private System.Windows.Forms.BindingSource humanBindingSource;
+        private databaseDataSetTableAdapters.HumanTableAdapter humanTableAdapter;
+        private System.Windows.Forms.BindingSource humanBindingSource1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Programma/ekzamen/ekzamen/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ekzamen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sql_connection;
+        string conn_string = @" Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\maksm\Documents\ekz\ekzamen\database.mdf; Integrated Security = True; Connect Timeout = 30";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "databaseDataSet.Human". При необходимости она может быть перемещена или удалена.
+            this.humanTableAdapter.Fill(this.databaseDataSet.Human);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "databaseDataSet.Company". При необходимости она может быть перемещена или удалена.
+            this.companyTableAdapter.Fill(this.databaseDataSet.Company);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM [HUMAN]";
+            try
+            {
+                sql_connection = new SqlConnection(conn_string);
+                sql_connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("нет подключения", "Сообщение", MessageBoxButtons.OK);
+            }
+
+
+        }
+    }
+}
